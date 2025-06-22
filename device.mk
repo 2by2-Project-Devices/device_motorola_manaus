89cd7248259df28670def547c97821832858aef7
electimon <electimon@gmail.com>
2025-06-22 20:14:18 +0900

    manaus: Add reversed egistec interface
    * Let's go with in-tree egistec interface.
    
    Change-Id: I8032b2865b7feb64bb72fbc37a5bcdcacffa3a5d

--- device.mk
+++ device.mk
@@ -137,7 +137,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.manaus
+    android.hardware.biometrics.fingerprint@2.3-service.manaus \
+    vendor.egistec.hardware.fingerprint@4.0.vendor
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
