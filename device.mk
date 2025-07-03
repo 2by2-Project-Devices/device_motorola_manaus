08697c2c3e5a09092913a317ba6c9dd7b1a3f94a
Roger Liao <rogerliao@google.com>
2025-07-02 23:13:17 -0500

    manaus: Add vndservicemanager to the product
    vndservicemanager has been removed for the devices with API level
    >= 30 but Redbull still needs it to provide vendor-implemented services.
    
    Ref: b/149867896
    Bug: 148823278
    Change-Id: I08ed6a242c8ac1f64da12204d618376fe1a5c8ac

--- device.mk
+++ device.mk
@@ -273,6 +273,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.mediatek
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service-lazy \
