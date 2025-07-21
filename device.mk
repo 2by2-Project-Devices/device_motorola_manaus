296c1acf2f82c1be2c4a73462b7df11f30a2479b
npjohnson <johnsonnolen@gmail.com>
2025-07-20 22:44:15 -0500

    manaus: Build moto configurable touch hal

--- device.mk
+++ device.mk
@@ -328,6 +328,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.manaus \
+    vendor.lineage.touch-service.motorola
+
+$(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
