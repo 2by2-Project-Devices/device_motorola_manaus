a63cc0db7dac55845a57eca2dfab29f1f4b7a0ff
bengris32 <bengris32@protonmail.ch>
2025-08-25 20:29:02 -0500

    manaus: Switch to pixel memtrack

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.memtrack-service.mediatek
+    android.hardware.memtrack-service.mediatek-mali
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
