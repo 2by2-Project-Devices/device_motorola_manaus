arrows-development/device_fcnt_fuji

manaus: Introduce biometric 2.3 hal
 * wrap 2.1 hal and add additional function

Change-Id: I4a66760fb8ca88de4e9229b7aec90052470ce6f0
Signed-off-by: electimon <electimon@gmail.com>

fingerprint/BiometricsFingerprint.cpp
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.manaus"
+
+#include "BiometricsFingerprint.h"
+
+#include <android-base/logging.h>
+#include <fstream>
+#include <cmath>
+#include <thread>
+
+#include <fcntl.h>
+#include <poll.h>
+#include <sys/stat.h>
+
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+namespace V2_3 {
+namespace implementation {
+
+BiometricsFingerprint::BiometricsFingerprint() {
+    biometrics_2_1_service = IBiometricsFingerprint_2_1::getService();
+}
+
+Return<uint64_t> BiometricsFingerprint::setNotify(const sp<IBiometricsFingerprintClientCallback>& clientCallback) {
+    return biometrics_2_1_service->setNotify(clientCallback);
+}
+
+Return<uint64_t> BiometricsFingerprint::preEnroll() {
+    return biometrics_2_1_service->preEnroll();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid, uint32_t timeoutSec) {
+    return biometrics_2_1_service->enroll(hat, gid, timeoutSec);
+}
+
+Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+    return biometrics_2_1_service->postEnroll();
+}
+
+Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
+    return biometrics_2_1_service->getAuthenticatorId();
+}
+
+Return<RequestStatus> BiometricsFingerprint::cancel() {
+    return biometrics_2_1_service->cancel();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enumerate() {
+    return biometrics_2_1_service->enumerate();
+}
+
+Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {
+    return biometrics_2_1_service->remove(gid, fid);
+}
+
+Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid, const hidl_string& storePath) {
+    return biometrics_2_1_service->setActiveGroup(gid, storePath);
+}
+
+Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId, uint32_t gid) {
+    return biometrics_2_1_service->authenticate(operationId, gid);
+}
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V2_3
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android

fingerprint/service.cpp
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.manaus"
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+#include "BiometricsFingerprint.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
+
+using android::OK;
+using android::status_t;
+
+int main() {
+    android::sp<IBiometricsFingerprint> service = new BiometricsFingerprint();
+
+    configureRpcThreadpool(1, true);
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register Biometrics 2.3 HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Biometrics 2.3 HAL service ready.";
+
+    joinRpcThreadpool();
+
+    LOG(ERROR) << "Biometrics 2.3 HAL service failed to join thread pool.";
+    return 1;
+}

2by2-Project-Devices/device_motorola_manaus

fingerprint-egis/BiometricsFingerprint.cpp
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.egis"
+
+#include "BiometricsFingerprint.h"
+
+#include <android-base/logging.h>
+#include <fstream>
+#include <cmath>
+#include <thread>
+
+#include <fcntl.h>
+#include <poll.h>
+#include <sys/stat.h>
+
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+namespace V2_3 {
+namespace implementation {
+
+BiometricsFingerprint::BiometricsFingerprint() {
+    biometrics_2_1_service = IBiometricsFingerprint_2_1::getService();
+}
+
+Return<uint64_t> BiometricsFingerprint::setNotify(const sp<IBiometricsFingerprintClientCallback>& clientCallback) {
+    return biometrics_2_1_service->setNotify(clientCallback);
+}
+
+Return<uint64_t> BiometricsFingerprint::preEnroll() {
+    return biometrics_2_1_service->preEnroll();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid, uint32_t timeoutSec) {
+    return biometrics_2_1_service->enroll(hat, gid, timeoutSec);
+}
+
+Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+    return biometrics_2_1_service->postEnroll();
+}
+
+Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
+    return biometrics_2_1_service->getAuthenticatorId();
+}
+
+Return<RequestStatus> BiometricsFingerprint::cancel() {
+    return biometrics_2_1_service->cancel();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enumerate() {
+    return biometrics_2_1_service->enumerate();
+}
+
+Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {
+    return biometrics_2_1_service->remove(gid, fid);
+}
+
+Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid, const hidl_string& storePath) {
+    return biometrics_2_1_service->setActiveGroup(gid, storePath);
+}
+
+Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId, uint32_t gid) {
+    return biometrics_2_1_service->authenticate(operationId, gid);
+}
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V2_3
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android

fingerprint-egis/service.cpp
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.egis"
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+#include "BiometricsFingerprint.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+using android::hardware::biometrics::fingerprint::V2_3::IBiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
+
+using android::OK;
+using android::status_t;
+
+int main() {
+    android::sp<IBiometricsFingerprint> service = new BiometricsFingerprint();
+
+    configureRpcThreadpool(1, true);
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register Biometrics 2.3 HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Biometrics 2.3 HAL service ready.";
+
+    joinRpcThreadpool();
+
+    LOG(ERROR) << "Biometrics 2.3 HAL service failed to join thread pool.";
+    return 1;
+}